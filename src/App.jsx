401ef79d9d44d3320e1f875c26a4a7c4ce91bc07
bibs <bibsbiana@protonmail.com>
2023-11-08 16:53:19 +0100

    Añadido funciones Filtrar y Reiniciar

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [filterInput, setFilterInput] = useState('');
+  const [userList] = useState([
+    'Maria Flores',
+    'Lola Perez',
+    'Encarna Asusnción',
+    'Julio Puertos',
+    'Luís Fontana',
+    'Manolo Gómez',
+  ]);
+  const [filteredUserList, setFilteredUserList] = useState([
+    'Maria Flores',
+    'Lola Perez',
+    'Encarna Asusnción',
+    'Julio Puertos',
+    'Luís Fontana',
+    'Manolo Gómez',
+  ]);
+
+  const filter = () => {
+    const tempUserList = [...userList];
+    const tempFilteredUserList = tempUserList.filter((user) =>
+      user.toLocaleLowerCase().includes(filterInput.toLocaleLowerCase())
+    );
+    setFilteredUserList(tempFilteredUserList);
+  };
+  const reset = () => {
+    setFilteredUserList(userList);
+    setFilterInput('');
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input type="button" value="Filtrar" onClick={() => filter()} />
+      <input type="button" value="Reiniciar" onClick={() => reset()} />
+      <input
+        type="text"
+        id="filter"
+        placeholder="Buscar..."
+        value={filterInput}
+        onChange={(e) => setFilterInput(e.target.value)}
+      />
+      <ul>
+        {filteredUserList.map((user) => (
+          <li key={user}>{user}</li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
